8a790025961dd61df46028e78bb4e479b8d5d64e
Iain Cruickshank <iain.j.cruickshank@gmail.com>
2022-09-09 15:46:41 -0400

    Update to url_utils init

---
+++ src/url_utils/__init__.py
@@ -0,0 +1,8 @@
+from .url_utils import internet_data_collection
+from .url_utils import get_domain
+from .url_utils importmap_mobile_to_original
+from .url_utils importremove_query_terms
+from .url_utils importget_base_url
+from .url_utils importprocess_urls
+from .url_utils importget_text_from_urls
+from .url_utils importget_images_from_urls
