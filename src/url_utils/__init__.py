2198dbda8124aefe73c6bb11f14868ed74a081db
Iain Cruickshank <iain.j.cruickshank@gmail.com>
2022-09-09 15:47:19 -0400

    Bug fix

--- src/url_utils/__init__.py
+++ src/url_utils/__init__.py
@@ -1,8 +1,8 @@
 from .url_utils import internet_data_collection
 from .url_utils import get_domain
-from .url_utils importmap_mobile_to_original
-from .url_utils importremove_query_terms
-from .url_utils importget_base_url
-from .url_utils importprocess_urls
-from .url_utils importget_text_from_urls
-from .url_utils importget_images_from_urls
+from .url_utils import map_mobile_to_original
+from .url_utils import remove_query_terms
+from .url_utils import get_base_url
+from .url_utils import process_urls
+from .url_utils import get_text_from_urls
+from .url_utils import get_images_from_urls
